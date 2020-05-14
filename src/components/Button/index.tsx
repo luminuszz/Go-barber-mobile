e5472f9c3f33bce992d17dd73c4747b12d40288b
luminuszz <luminuszz43@gmail.com>
2020-05-14 16:18:14 -0300

    finalizando tela de login e cadastro

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+import { Container, ButtonText } from './styles';
+
+interface ButtonsProps extends RectButtonProperties {
+  children: string;
+}
+
+const Button: React.FC<ButtonsProps> = ({ children, ...rest }) => {
+  const teste = 0;
+
+  return (
+    <Container {...rest}>
+      <ButtonText>{children}</ButtonText>
+    </Container>
+  );
+};
+
+export default Button;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+
+import { Container, TextInput, Icon } from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+  icon: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+  const teste = 0;
+
+  return (
+    <Container>
+      <Icon name={icon} size={20} />
+      <TextInput
+        keyboardAppearance="dark"
+        placeholderTextColor="#666360"
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default Input;
